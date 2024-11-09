c3512888562f68cbc91f9a551255cf50ca3feeae
Rasul <mrgaaksm@gmail.com>
2024-11-09 18:53:10 +0300

    splash

--- app/src/main/java/com/example/ocsapp/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/ocsapp/activities/SplashScreenActivity.kt
@@ -1,21 +1,36 @@
 package com.example.ocsapp.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.slidingpanelayout.widget.SlidingPaneLayout.LayoutParams
 import com.example.ocsapp.R
 
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val cont: RelativeLayout = findViewById(R.id.cont)
+
+
+        cont.alpha = 0f
+        cont.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this, OnBoardActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
         }
+
     }
 }
