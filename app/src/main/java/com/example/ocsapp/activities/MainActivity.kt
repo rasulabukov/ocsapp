dde0ad905e12244a9e80eb5de376447879e97a00
Rasul <mrgaaksm@gmail.com>
2024-11-09 13:04:20 +0300

    menu

--- app/src/main/java/com/example/ocsapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/ocsapp/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -15,28 +16,20 @@ import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var toggle: ActionBarDrawerToggle
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val drawerLayout: DrawerLayout = findViewById(R.id.main)
         val navView: NavigationView = findViewById(R.id.navigation_view)
+        val imgButt: ImageButton = findViewById(R.id.drawer_button)
 
-        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
-            return true
+        imgButt.setOnClickListener {
+            drawerLayout.open()
         }
-        return super.onOptionsItemSelected(item)
+
+
     }
+
 
 }
